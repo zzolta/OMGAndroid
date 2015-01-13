7d90215f86b9a1dc293916c7cf74182ff68e0084
Szekely, Zolta <Zolta.Szekely@wolterskluwer.com>
2015-01-13 19:37:30 +0200

    Added a textview and a button with an action.

--- app/src/main/java/com/example/zzolta/omgandroid/MainActivity.java
+++ app/src/main/java/com/example/zzolta/omgandroid/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.zzolta.omgandroid;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    TextView mainTextView;
+    Button mainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainTextView.setText("Set in Java!");
+
+        mainButton = (Button) findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
     }
 
     @Override
@@ -19,5 +30,10 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        mainTextView.setText("Button pressed!");
     }
 }
