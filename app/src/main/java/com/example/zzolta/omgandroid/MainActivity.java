9ef697bd58fa8a5d6dc4e6435f296aac948932e5
Szekely, Zolta <Zolta.Szekely@wolterskluwer.com>
2015-01-13 20:49:12 +0200

    Added display welcome functionality.

--- app/src/main/java/com/example/zzolta/omgandroid/MainActivity.java
+++ app/src/main/java/com/example/zzolta/omgandroid/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.zzolta.omgandroid;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
@@ -15,11 +18,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+
+    private static final String PREFS = "prefs";
+    private static final String PREF_NAME = "name";
 
     TextView mainTextView;
     Button mainButton;
@@ -28,6 +35,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     ArrayAdapter<String> mArrayAdapter;
     ArrayList<String> mNameList = new ArrayList<>();
     ShareActionProvider mShareActionProvider;
+    SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +56,45 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
         mainListView.setAdapter(mArrayAdapter);
         mainListView.setOnItemClickListener(this);
+
+        displayWelcome();
+    }
+
+    private void displayWelcome() {
+        mSharedPreferences = getSharedPreferences(PREFS, MODE_PRIVATE);
+
+        String name = mSharedPreferences.getString(PREF_NAME, "");
+
+        if (name.length() > 0) {
+            Toast.makeText(this, "Welcome back, " + name + "!", Toast.LENGTH_LONG).show();
+        } else {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Hello!");
+            alert.setMessage("What is your name?");
+
+            final EditText input = new EditText(this);
+            alert.setView(input);
+
+            alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String inputName = input.getText().toString();
+                    SharedPreferences.Editor editor = mSharedPreferences.edit();
+                    editor.putString(PREF_NAME, inputName);
+                    editor.commit();
+
+                    Toast.makeText(getApplicationContext(), "Welcome, " + inputName + "!", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            });
+
+            alert.show();
+        }
     }
 
     @Override
