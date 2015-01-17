04a35cb5e3297bc7742ed40f59c162c198676245
Szekely, Zolta <Zolta.Szekely@wolterskluwer.com>
2015-01-17 15:58:49 +0200

    Added functionality to share details of books.

--- app/src/main/java/com/example/zzolta/omgandroid/DetailActivity.java
+++ app/src/main/java/com/example/zzolta/omgandroid/DetailActivity.java
@@ -1,7 +1,12 @@
 package com.example.zzolta.omgandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -12,7 +17,9 @@ import com.squareup.picasso.Picasso;
 public class DetailActivity extends ActionBarActivity {
 
     private static final String IMAGE_URL_BASE = "http://covers.openlibrary.org/b/id/";
+
     String mImageURL;
+    ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +38,29 @@ public class DetailActivity extends ActionBarActivity {
         }
 
         Picasso.with(this).load(mImageURL).placeholder(R.drawable.img_books_loading).into(imageView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+
+        if (shareItem != null) {
+            mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        }
+
+        setShareIntent();
+
+        return true;
+    }
+
+    private void setShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Book Recommendation!");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mImageURL);
+
+        mShareActionProvider.setShareIntent(shareIntent);
     }
 }
